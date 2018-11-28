674b509f6aa5c8299d61b6a8b956a549ca1838f5
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-28 12:10:13 +0600

    Fix frontend and create auth scafolding

---
+++ resources/views/index.blade.php
@@ -0,0 +1,278 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS -->
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous"> --}}
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/frontend/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="/frontend/css/bootstrap.css">
+    <link rel="stylesheet" href="/frontend/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="/frontend/css/bootstrap-grid.css.map">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-grid.min.css.map">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-reboot.css.map">
+    <link rel="stylesheet" href="/frontend/css/bootstrap-reboot.min.css.map">
+    <link rel="stylesheet" href="/frontend/css/bootstrap.css.map">
+    <link rel="stylesheet" href="/frontend/css/bootstrap.min.css.map">
+
+    <link rel="stylesheet" type="text/css" href="/frontend/css/style.css">
+
+    <link rel="shortcut icon" type="image/png" href="/frontend/Assets/04 Logo/MONGOL TORI LOGO FINAL.png"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Chakra+Petch" rel="stylesheet">
+    <title>Mongol Tori</title>
+</head>
+<body>
+    <section class="intro">
+        <div class="container-fluid" id="logo-container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <img src="/frontend/Assets/04 Logo/MONGOL TORI LOGO FINAL.png" id="">
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar-nav navbar-expand-lg navbar-light bg-transparent text-center">
+            <a href="#about-us" class="navbar-brand">About</a>
+            <a href="#work" class="navbar-brand">Work</a>
+            <a href="#team" class="navbar-brand">Team</a>
+            <a href="#sponsors" class="navbar-brand">Sponsors</a>
+            <a href="#contact" class="navbar-brand">Contact Us</a>
+        </nav>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10"><p id="spp" style="font-size: 3.5vw;"><strong>SUCCESS.<br>PASSION.<br>PERFORMANCE.</strong></p></div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>       
+    </section> <!-- intro -->
+            
+    <section class="about-us section" id="about-us">
+        <div class="container">
+            <h1 style="left: 12%; position: relative;"><strong>About Us</strong></h1>
+            <br>
+            <div class="row">
+                <div class="col-md-6">
+                    <p id="mongoltori">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-5">
+                    <img src="/frontend/img/mars logo.png" align="right">
+                </div>
+            </div> <!--row -->
+            <br><br>
+            <div class="row">
+                <div class="col-sm-10">
+                    <iframe id="video" class="embed-responsive embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </div>
+            </div>
+        </div>
+    </section> <!-- about-us -->
+                
+    <section class="our-works" id="work">
+
+        <div class="container-fluid">
+            <div class="row">
+                <h1 style="font-size: 3vw; color: #fff; left: 20%; position: relative;"><strong>Our Works</strong></h1>
+            </div> <!-- row -->
+
+            <div class="row">
+                <div class="col-sm-8">
+                <p style="color: #fff; position: relative; left: 30%;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img class="d-block w-50" src="/frontend/img/interceptor.jpg" alt="Interceptor" width="450px" height="400px">
+                            <div class="carousel-caption">
+                                <h5 align="center">Interceptor<br>(Battlebot) 2016</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-50" src="/frontend/img/PathFinder.jpg" alt="Pathfinder" width="450px" height="400px">
+                            <div class="carousel-caption">
+                                <h5 align="center">Pathfinder<br>(Follower) 2015</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-50" src="/frontend/img/MT.jpg" alt="Mongol Tori" width="450px" height="400px">
+                            <div class="carousel-caption">
+                                <h5 align="center">Mongol Tori<br>(Boat of Mars) 2017</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>                
+        </div>
+    </section> <!-- Our Works -->
+                    
+    <section class="team" style="padding: 50px;" id="team">
+        <div class="text-center pt-4">
+            <h4 style="padding-bottom: 30px; font-size: 40px;"><strong>Meet Our Team</strong></h4>
+        </div>
+            
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <div class="member-1">
+                        <div class="avatar">
+                            <span><img src="/frontend/img/Dr. Md. Khalilur Rhaman.jpg" alt="avatar"></span>
+                        </div>
+                            <div class="desc">
+                            <h5 class="text-dark">Dr. Md. Khalilur Rahman</h5>
+                            <h6 class="text-dark">Faculty Adviser</h6>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="member-2">
+                        <div class="avatar">
+                            <span><img src="/frontend/img/Md. Saiful Islam.jpg" alt="avatar"></span>
+                        </div>
+                        <div class="desc">
+                            <h5 class="text-dark">Md. Saiful Islam</h5>
+                            <h6 class="text-dark">Faculty Adviser</h6>
+                        </div>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="member-3">
+                        <div class="avatar">
+                            <span><img src="/frontend/img/Abdullah.jpg" alt="avatar"></span>
+                        </div>
+                        <div class="desc">
+                            <h5 class="text-dark">Abdullah</h5>
+                            <h6 class="text-dark">Faculty Adviser</h6>
+                        </div>
+                    </div>
+                </div>        
+            </div>
+            <br><br>
+            <div class="row ">
+                <div class="col-md-12 text-center">
+                    <button type="submit" id="teamButton"><strong>View Full Team</strong></button>
+                </div>
+            </div>
+        </div>
+    </section> <!-- team -->
+            
+    <section class="sponsor" id="sponsors">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h1>Sponsored By</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <img src="/frontend/img/bracu.png" style="height: 130px; width: 150px; position: relative; left: 43%; top: 10px;" align="center">
+            </div>
+
+            <div class="row">  
+                <img src="/frontend/img/bracbank.jpg" class="first-line-image">
+                <img src="/frontend/img/square.png" class="first-line-image">
+                <img src="/frontend/img/grameenintel.jpg" class="first-line-image">
+                <img src="/frontend/img/prothomalo.png" class="first-line-image">
+            </div>
+
+            <div class="row">
+                <img src="/frontend/img/cytron.png" class="second-line-image">
+                <img src="/frontend/img/eximbank.png" class="second-line-image">
+                <img src="/frontend/img/fsib.jpg" class="second-line-image" style="width: 115px;">
+                <img src="/frontend/img/robu.png" class="second-line-image" style="width: 115px;">
+                <img src="/frontend/img/naster.png" class="second-line-image">
+            </div>
+        </div>
+    </section> <!-- sponsor -->
+            
+    <section class="contact-us">
+        <div class="container">
+            <h1 style="color: #fff; padding-top: 35px;" align="center">Contact Us</h1>
+                    
+            <form>
+                <div class="form-row">
+                    <div class="form-group col">
+                        <input type="email" class="form-control transparent-input" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col">
+                        <input type="password" class="form-control transparent-input" id="inputPassword4" placeholder="Password">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <textarea class="form-control transparent-input" id="exampleFormControlTextarea1" rows="7" placeholder="Write your message here..."></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary" id="submitButton" align="center"><strong>Submit</strong></button>
+            </form>
+
+            <div class="text-center pt-4">
+                <h4 style="padding-top: 30px; font-size: 20px; color: #fff"><strong>Connect Us With</strong></h4>
+            </div>
+            <div class="social-icon text-center">
+                <span><i class="fab fa-facebook-f"></i></span>
+                <span><i class="fab fa-twitter"></i></span>
+                <span><i class="fab fa-linkedin"></i></span>
+                <span><i class="fab fa-google-plus"></i></span>
+            </div>
+        </div>
+    </section> <!--contact us-->
+            
+    <section class="footer">
+        <div class="container">
+            <div class="row" style="padding-top: 20px;">
+                <div class="col-md-8 text-center">
+                    <p>&copy;Copyright BRACU Mongol-Tori 2017-18 | Privacy Policy | Terms of Use</p>
+                </div>
+                <div class="col-md-4">
+                    <p>Made with <i class="fas fa-heart"></i></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    {{-- <script src="/frontend/https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="/frontend/https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="/frontend/https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> --}}
+</body>
+</html>
