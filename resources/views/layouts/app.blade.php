b4d39dbcf28398455583cf68874b55cf8ea86772
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2018-11-29 09:58:58 +0600

    Design database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
+    <link rel="shortcut icon" type="image/png" href="/frontend/Assets/04 Logo/MONGOL TORI LOGO FINAL.png"/>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
